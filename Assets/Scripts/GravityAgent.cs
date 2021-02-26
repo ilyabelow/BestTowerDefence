80e7d3d090620ff0c3ac0113290cd1dd564acac2
ilyabelow <ilyabelow.almost.surely@gmail.com>
2021-02-26 12:59:06 +0300

    better gravity

--- Assets/Scripts/GravityAgent.cs
+++ Assets/Scripts/GravityAgent.cs
@@ -5,42 +5,41 @@ using UnityEngine;
 using UnityEngine.Serialization;
 using Object = System.Object;
 
-public class Gravity : MonoBehaviour
+public class GravityAgent : MonoBehaviour
 {
     [SerializeField] 
-    private float massRatio;
-    [SerializeField]
-    private float gravityConstant;
+    private float mass;
+    [SerializeField] 
+    private float gravityConstant; // TODO make common
     [SerializeField]
     private Vector3 startVelocity;
-
+    [SerializeField]
+    private GravityAgent companion;
+    
     private Vector3 velocity;
+    
 
-    private GameObject sun;
     
     private void Start()
     {
         velocity = startVelocity;
-        sun = GameObject.Find("/GravityTask/sun");
-        
-
     }
 
     void FixedUpdate()
     {
-        Vector3 direction =  sun.transform.position - transform.position;
+        Vector3 direction =  companion.transform.position - transform.position;
         float Rr = direction.magnitude;
 
-        if (Rr < (transform.localScale.x + sun.transform.localScale.x)*.5) // Collision
+        if (Rr < (transform.localScale.x + companion.transform.localScale.x)*.5) // Collision
         {
             Destroy(gameObject);
             Debug.Log("Boom!");
             return;
         }
         
-        Vector3 force = gravityConstant * massRatio / (Rr * Rr * Rr) * direction;
+        Vector3 acc = gravityConstant * companion.mass / (mass * Rr * Rr * Rr) * direction;
 
-        velocity += force * Time.deltaTime;
+        velocity += acc * Time.deltaTime;
         transform.position += velocity * Time.deltaTime;
 
     }
